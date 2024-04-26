072aef35f0a6daec1aff4b87812522f71da423f6
codecraftwt <codecraftwt@gmail.com>
2024-04-26 19:00:03 +0530

    url update3

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'file:///D:/Test/AstroThemeUpload/public/index-v2.html',
+    site: 'https://expertcare.github.io/AstroThemeUpload/index-v2.html',
 });
