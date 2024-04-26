34e051dfc4d46130f1fa3e85eb6a8ec6ab889c37
codecraftwt <codecraftwt@gmail.com>
2024-04-26 16:11:34 +0530

    yml removed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-  site: 'https://expertcare.github.io',
+  site: 'https://github.com/expertcare/AstroThemeUpload.git',
   base: 'AstroThemeUpload',
+  buildOptions: {
+    // Output the built files into the docs folder
+    out: 'docs',
+  },
 })
