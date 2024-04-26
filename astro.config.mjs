129cdb2365c257abf3d168514e9b237a72c5149e
codecraftwt <codecraftwt@gmail.com>
2024-04-26 18:12:04 +0530

    files added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,4 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
-export default defineConfig({
-  site: 'https://github.com/expertcare/AstroThemeUpload.git',
-  base: 'AstroThemeUpload',
-  buildOptions: {
-    // Output the built files into the docs folder
-    out: 'docs',
-  },
-})
+// https://astro.build/config
+export default defineConfig({});
